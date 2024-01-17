fcbe0698252c7fbfa56439818021348b3db51ff7
bekkaze <bekkaze7@gmail.com>
2024-01-17 13:25:27 +0800

    New

--- frontend/src/components/dashboard/Sidebar.jsx
+++ frontend/src/components/dashboard/Sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar() {
       <div className='flex items-center gap-2 px-1 py-3'>
         <BsBoxSeamFill fontSize={24}/>
         <span className='text-slate-100' text-lg>AbuseBox</span>
-      </div>
+      </div> 
       <div className='flex-1 py-8 flex flex-col gap0.5'>
         {DASHBOARD_SIDEBAR_LINKS.map( (item) => (
           <SidebarLink key={item.key} item={item}/>
